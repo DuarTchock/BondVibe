1f25fee5eddf52ad35130e528178027220e962f4
Carlos Duarte <jcpuntoduarte@gmail.com>
2026-01-04 17:49:28 -0500

    chore: remove temporary fix scripts

--- fix-line.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = 'src/screens/EventDetailScreen.js';
-let lines = fs.readFileSync(path, 'utf8').split('\n');
-
-// Línea 1008 (índice 1007)
-lines[1007] = '                    <AvatarDisplay avatar={attendee.avatar || { type: "emoji", value: attendee.emoji || "😊" }} size={36} />';
-
-fs.writeFileSync(path, lines.join('\n'));
-console.log('✅ Fixed line 1008');
